d0064032661a95ba76c716b47adcb529b02b38e7
andre <andreytr449@gmail.com>
2025-04-07 22:14:22 +0300

    feat: implement username creation screen

--- app/auth/sign-in.tsx
+++ app/auth/sign-in.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import EmailAuthFormScreen from "@/screens/email-auth-form-screen";
+import EmailAuthFormScreen from "@/screens/email-auth-form.screen";
 
 const SignIn = () => {
     return (
--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -10,10 +10,10 @@ interface InputProps extends TextInputProps {
 
 const Input = ({ className, label, ...rest }:InputProps) => {
     const {isDarkMode} = useTheme()
-    let styles = isDarkMode ? 'bg-surfaces-dark-1 border-border-dark' : 'bg-surfaces-light-1 border-border-light'
+    let styles = isDarkMode ? 'bg-surfaces-dark-1 border-border-dark text-bg-light' : 'bg-surfaces-light-1 border-border-light text-bg-dark'
     return (
         <View className='my-3'>
-            <Text className={`text-body-small ${isDarkMode ? 'text-body-primary-dark' : 'text-body-primary-light'}`}>{label}</Text>
+            <Text className={`mb-2 font-light text-body-small ${isDarkMode ? 'text-body-primary-dark' : 'text-body-primary-light'}`}>{label}</Text>
         <TextInput className={`${styles}  rounded-[12px] border py-3 px-4`} placeholderTextColor={isDarkMode ? '#4C4C4C' :'#D7D7D7'} {...rest} />
         </View>
     );
