d749bec6fa93d574f5fa0b7190939d24e59024ec
andre <andreytr449@gmail.com>
2025-04-25 19:13:59 +0300

    fix: updated toogle theme logic

--- store/theme.ts
+++ store/theme.ts
@@ -1,19 +1,22 @@
-import {create} from "zustand";
+import { create } from "zustand";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 type useThemeType = {
-    isDarkMode: boolean
-    toggleTheme: (theme: 'dark' | 'light' | null | undefined) => void
-}
+    isDarkMode: boolean;
+    toggleTheme: (theme: 'dark' | 'light' | null | undefined) => void;
+};
 
 const useTheme = create<useThemeType>((set) => ({
     isDarkMode: false,
-    toggleTheme: (theme) => {
-        if (theme === 'dark') {
-            set({isDarkMode: true});
-        } else {
-            set({isDarkMode: false});
+    toggleTheme: async (theme) => {
+        const isDark = theme === 'dark';
+        set({ isDarkMode: isDark });
+        try {
+            await AsyncStorage.setItem('appTheme', isDark ? 'dark' : 'light');
+        } catch (e) {
+            console.warn("Error with toogle theme:", e);
         }
-    }
-}))
+    },
+}));
 
-export default useTheme
+export default useTheme;
